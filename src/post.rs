14c351f7b850aa675f6a260366acd87ff2c05d7f
mbiti2 <louisembiti@gmail.com>
2025-03-01 11:40:43 +0100

    Update post.rs

--- src/post.rs
+++ src/post.rs
@@ -4,18 +4,17 @@ use reqwest::{header, Client, Error, Response, Url};
 use serde::Serialize;
 #[derive(Serialize)]
 struct Comment {
-    body: String,
+    body: Vec<String>,
 }
 
 #[tokio::main]
-pub async fn post_comment(fib_num: u128, owner: &str, repo: &str, pr_num: u32, token: String ) -> Result<String, Error> { 
+pub async fn post_comment( owner: &str, repo: &str, pr_num: u32, token: String, comments: Vec<String>) -> Result<Vec<String>, Error> { 
     // https://api.github.com/repos/FibBot/issues/1/comments
 
     let client = Client::new();
 
 
-    let comment = format!("The fibonnaci number is : {}", fib_num);
-    let comment = Comment { body: comment.to_string() };
+    let comment = Comment { body: comments };
     
     let response = client
     .post(format!("https://api.github.com/repos/{owner}/{repo}/issues/{pr_num}/comments" ))
